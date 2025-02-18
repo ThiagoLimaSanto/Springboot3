2713e3901c1d7b967b58c716815611cfbdb8a09b
Thiago Lima Santos <tlima7601@gmail.com>
2025-02-18 15:50:35 -0300

    Project created

---
+++ src/main/java/com/educandoweb/aulaspringboot/AulaspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.educandoweb.aulaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaspringbootApplication.class, args);
+	}
+}
