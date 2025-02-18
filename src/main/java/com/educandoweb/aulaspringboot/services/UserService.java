98c1eccfe86f2bcba58bfb0653f7dcf0be150dcf
Thiago Lima Santos <tlima7601@gmail.com>
2025-02-18 17:12:03 -0300

    Service layer, componet registration

---
+++ src/main/java/com/educandoweb/aulaspringboot/services/UserService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.aulaspringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.aulaspringboot.entities.User;
+import com.educandoweb.aulaspringboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findByID(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
