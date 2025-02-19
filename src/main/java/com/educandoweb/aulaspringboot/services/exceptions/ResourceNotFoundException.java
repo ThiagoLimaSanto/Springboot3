543ac06e62c9fd9e7df1ff420237ea5ca6332f64
Thiago Lima Santos <tlima7601@gmail.com>
2025-02-19 16:20:04 -0300

    Exception handling

---
+++ src/main/java/com/educandoweb/aulaspringboot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.educandoweb.aulaspringboot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. id " + id);
+    }
+}
